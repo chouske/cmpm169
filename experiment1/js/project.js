3ba27032a54da1d36840b90c77d39885fe4f536a
chouske <132114322+chouske@users.noreply.github.com>
2024-04-06 12:42:32 -0700

    experiment1 done

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,71 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    adventurer: ["My dude", "Bro", "WesBot", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "勇者", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
+    pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
+    post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
+    people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca", "帥氣"],
+    item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
+    num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
+    looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
+    loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
+    baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
+    message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
+    
+  };
+  const frogFillers = {
+    locations: ["wetlands", "forest"],
+    animals: ["frog", "crane", "turtle", "fish"],
+    spots: ["lilypad", "tree", "plant", "rock"],
+    weather: ["shady", "sunny", "windy"],
+    adjectives: ["worst", "best", "funniest", "most tragic"],
+    actions: ["cry", "smile", "laugh", "scream"],
+    animalNames: ["Peter", "Rachel", "Barack", "Terrance"],
+    foods: ["cupcakes", "tbone steaks", "pizzas", "chocolates", "ribs"]
+  }
+  const template = `$adventurer, heed my $message!
+  
+  I have just come from $pre$post where the $people folk are in desperate need. Their town has been overrun by $baddies. You must venture forth at once, taking my $item, and help them.
+  
+  It is told that the one who can rescue them will be awarded with $num $looty $loots. Surely this must tempt one such as yourself!
+  `;
+  const frogTemplate = `It's another $weather day in the $locations!
+  
+  Benny the frog is quite the jokester. While they were at the $spots Benny told $animalNames the $animals the $adjectives joke ever, making them $actions! 
+  
+  Due to this they decided to meet $animalNames the $animals and buy expensive $foods at the $weather $spots restaurant. Their lives were the $adjectives I've ever heard of! The end
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = frogFillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = frogTemplate;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate(); 
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
